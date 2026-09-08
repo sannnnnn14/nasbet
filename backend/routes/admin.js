400fdaed61e990934edaecd3db9bb7d293a8dffa
sannnnnn14 <sanranj591@gmail.com>
2026-09-08 03:26:44 +0300

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Transaction, User } from '../models.js';
-import { auth, admin } from '../middleware/admin.js';
+import { auth } from '../middleware/auth.js';
+import { admin } from '../middleware/admin.js';
 
 const router = express.Router();
 router.use(auth);
